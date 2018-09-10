0b04fe405e1803425600ddec733ddb958de7e090
Unknown <sedrickidd@hotmail.com>
2018-09-10 15:17:37 -0400

    Ajout de la classe dao
    contien le constructeur le get instance et une fonction test

---
+++ src/donnee/JeuDAO.java
@@ -0,0 +1,55 @@
+package donnee;
+
+import modele.Jeu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JeuDAO {
+    public List<Jeu> simulerListeJeu()
+    {
+        List<Jeu> listeJeu = new ArrayList<>();
+        listeJeu.add(new Jeu("Death stranding", "un jeu de Hideo Kojima"));
+        listeJeu.add(new Jeu("Forza horizon 4", "Un nouveau jeu de course marqué Microsoft"));
+        return listeJeu;
+    }
+
+    private static String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+    private static String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/bergerie";
+    private static String BASEDEDONNEES_USAGER = "postgres";
+    private static String BASEDEDONNEES_MOTDEPASSE = "test";
+    private Connection connection = null;
+    public static JeuDAO instance = null;
+
+    public JeuDAO()
+    {
+        try {
+            Class.forName(BASEDEDONNEES_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JeuDAO getInstance()
+    {
+        if(null == instance)
+        {
+            instance = new JeuDAO();
+        }
+        return instance;
+    }
+
+
+
+
+
+
+}
