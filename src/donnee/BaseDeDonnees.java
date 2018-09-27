d712411373f0b4f19047c47b9b158db6fac40af4
Sedrick <sedrickidd@hotmail.com>
2018-09-27 14:57:18 -0400

    Création de la classe baseDeDonnees et transfer de la connection

---
+++ src/donnee/BaseDeDonnees.java
@@ -0,0 +1,4 @@
+package donnee;
+
+public class BaseDeDonnees {
+}
--- src/donnee/JeuDAO.java
+++ src/donnee/JeuDAO.java
@@ -15,25 +15,13 @@ public class JeuDAO {
         return listeJeu;
     }
 
-    private static String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
-    private static String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/jeu_interessant";
-    private static String BASEDEDONNEES_USAGER = "postgres";
-    private static String BASEDEDONNEES_MOTDEPASSE = "test";
+
     private Connection connection = null;
     public static JeuDAO instance = null;
 
     public JeuDAO()
     {
-        try {
-            Class.forName(BASEDEDONNEES_DRIVER);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+       this.connection = BaseDeDonnees.getInstance().getConnection();
     }
 
     public static JeuDAO getInstance()
